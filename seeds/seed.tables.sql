22ee08a30ba209467834f6c7ee6bb561059d8a03
M Jones <jonesman2800@gmail.com>
2020-07-27 14:07:38 -0400

    updated seed file for asl, uploaded signing images
    Co-authored-by: SDMcCarty <dragonflyakua@gmail.com>

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -17,18 +17,20 @@ VALUES
 
 INSERT INTO "language" ("id", "name", "user_id")
 VALUES
-  (1, 'French', 1);
+  (1, 'ASL', 1);
 
 INSERT INTO "word" ("id", "language_id", "original", "translation", "next")
 VALUES
-  (1, 1, 'entraine toi', 'practice', 2),
-  (2, 1, 'bonjour', 'hello', 3),
-  (3, 1, 'maison', 'house', 4),
-  (4, 1, 'développeur', 'developer', 5),
-  (5, 1, 'traduire', 'translate', 6),
-  (6, 1, 'incroyable', 'amazing', 7),
-  (7, 1, 'chien', 'dog', 8),
-  (8, 1, 'chat', 'cat', null);
+  (1, 1, 'src\images\tinified-10signs\1.jpg', 'again', 2),
+  (2, 1, 'src\images\tinified-10signs\2.jpg', 'good', 3),
+  (3, 1, 'src\images\tinified-10signs\3.jpg', 'hello', 4),
+  (4, 1, 'src\images\tinified-10signs\4.jpg', 'I don''t understand', 5),
+  (5, 1, 'src\images\tinified-10signs\5.jpg', 'I''m sorry', 6),
+  (6, 1, 'src\images\tinified-10signs\6.jpg', 'no', 7),
+  (7, 1, 'src\images\tinified-10signs\7.jpg', 'please', 8),
+  (8, 1, 'src\images\tinified-10signs\8.jpg', 'see you later', 9),
+  (9, 1, 'src\images\tinified-10signs\9.jpg', 'thank you', 10),
+  (10, 1, 'src\images\tinified-10signs\10.jpg', 'yes', null);
 
 UPDATE "language" SET head = 1 WHERE id = 1;
 
